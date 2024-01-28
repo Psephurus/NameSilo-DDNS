4b3e41ced880714b6dff2277879aa683e7ae0f17
Psephurus <ni3tnt@hotmail.com>
2024-01-29 01:09:03 +0800

    Add IPv4 support

--- NameSilo-DDNS.ps1
+++ NameSilo-DDNS.ps1
@@ -1,19 +1,68 @@
 # NameSilo DDNS PowerShell script
 
 # Domain name and API key variables
-$APIkey = "n0d1vn2v2aaetfudk0a12t”
-$domain = "mydomain.xyz"
-$record = "subdomain"
+$config = Get-Content -Raw -Path "conf\config.json" | ConvertFrom-Json
+$APIkey = $config.APIkey
+$domain = $config.domain
+$record = $config.record
+
+function Get-IPAddress {
+    # Get IPv6 address for Ethernet interface (if available)
+    $ethernetIP = Get-NetIPAddress -AddressFamily IPv6 |
+        Where-Object {
+            $_.InterfaceAlias -like '*Ethernet*' -or
+            $_.InterfaceAlias -like '*以太网*' -or
+            $_.InterfaceAlias -like '*이더넷*'
+        } |
+        Select-Object -Last 1 -ErrorAction SilentlyContinue
+
+    # Get IPv6 address for WLAN interface
+    $wlanIP = Get-NetIPAddress -AddressFamily IPv6 |
+               Where-Object {
+                   $_.InterfaceAlias -like '*WLAN*' -and
+                   $_.SuffixOrigin -eq 'Link' -or
+                   $_.InterfaceAlias -like '*Wi-Fi*'
+                } |
+               Select-Object -Last 1 -ErrorAction SilentlyContinue
+
+    # Check if Ethernet IPv6 address is available
+    if ($ethernetIP.IPAddress -and $ethernetIP.IPAddress -notlike "fe80::*") {
+        Write-Output $ethernetIP.IPAddress
+    }
+    # Check if WLAN IPv6 address is available
+    elseif (
+        $wlanIP.IPAddress -and 
+        ($wlanIP.IPAddress -notlike "127.*" -and 
+         $wlanIP.IPAddress -notlike "10.*" -and 
+         $wlanIP.IPAddress -notlike "192.168.*")
+    ) {
+        Write-Output $wlanIP.IPAddress
+    }
+}
 
 # Get IP address
-$listdomains = irm -Uri "https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=$APIkey&domain=$domain"
-$CurrentIP = (ipconfig|findstr "IPv6")[0].split()[-1]
-$RecordIP = ($listdomains.namesilo.reply.resource_record|where {$_.host -eq "$record.$domain"}).value
-$RecordID = ($listdomains.namesilo.reply.resource_record|where {$_.host -eq "$record.$domain"}).record_id
+$listdomains = Invoke-RestMethod -Uri `
+    "https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=$APIkey&domain=$domain"
+$CurrentIP = Get-IPAddress
+$RecordIP = $listdomains.namesilo.reply.resource_record |
+    Where-Object { $_.host -eq "$record.$domain" } |
+    Select-Object -ExpandProperty value
+$RecordID = $listdomains.namesilo.reply.resource_record |
+    Where-Object { $_.host -eq "$record.$domain" } |
+    Select-Object -ExpandProperty record_id
 $listdomains.namesilo.reply
 
 # Update IP address
 if ($CurrentIP -ne $RecordIP){
-$update = irm -Uri "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$APIkey&domain=$Domain&rrid=$RecordID&rrhost=$record&rrvalue=$CurrentIP&rrttl=3600"
-$update.namesilo.reply
+    $rrttl = $config.ttl # TTL Time
+    $updateUri = "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml" +
+        "&key=$APIkey" +
+        "&domain=$Domain" +
+        "&rrid=$RecordID" +
+        "&rrhost=$record" +
+        "&rrvalue=$CurrentIP" +
+        "&rrttl=$rrttl"
+    $update = Invoke-RestMethod -Uri $updateUri
+
+    $update.namesilo.reply
 }
--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # NameSilo-DDNS
 
-A PowerShell script to update Namesilo's DNS record when IP changed.
+A PowerShell script to update Namesilo's DNS record when IP changes (mostly used on Windows).
 
-## Usage: 
-- Generate API key in the API MANAGER at Namesilo, and save it to a save place.
-- copy the script to somewhere.
-- set $APIKey, $domain, and $record in the script.
-- Create a task scheduler.
+## Usage
 
-### Set a task
+1. Generate an API key in the [API MANAGER](https://www.namesilo.com/account/api-manager) at Namesilo and save it in a secure location.
+2. Copy the script to a desired location.
+3. Rename `conf.json.example` to `conf.json` and configure the parameters.
+4. Create a task scheduler.
 
-1. Enter "Task Scheduler" in the search box and open it.
+### Set up a Task in Task Scheduler
+
+1. Open "Task Scheduler" (e.g., by searching for it in the Start menu).
 2. Click "Create Task".
-3. Name the Task as "NameSilo-DDNS" && Check "Run whether user is logged on or not".
-4. Set trigger. Set Schedule to "Daily", and Repeat the task every "1 hour" for a duration of "1 day".
-5. Set Action. Type "powershell.exe" in the Program/script textbox. And in the "Add Arguments" box type `-ExecutionPolicy Bypass <script location>\NameSiloDDNS.ps1"`
+3. Name the Task as "NameSilo-DDNS" (or any preferred name) and Check "Run whether user is logged on or not".
+4. Set the trigger: Set Schedule to "Daily", and Repeat the task every "1 hour" for a duration of "1 day".
+5. Set the Action:
+    - Type "powershell.exe" (or "pwsh" for the new PowerShell) in the Program/script textbox.
+    - In the "Add Arguments" box, type `-ExecutionPolicy Bypass -File <script location>\NameSiloDDNS.ps1"`
